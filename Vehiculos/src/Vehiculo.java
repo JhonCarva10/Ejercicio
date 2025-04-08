a55c5b391e241917eb49cd44988c59679fd60fb6
Jhon Carvajal <jcarvajal@smart-financial-systems.com>
2025-04-08 10:34:38 -0500

    Se creó la clase vehículo con sus atributos y métodos

--- Vehiculos/src/Vehiculo.java
+++ Vehiculos/src/Vehiculo.java
@@ -1,2 +1,18 @@
-public class Vehiculo {
+public abstract class Vehiculo {
+    protected int velocidadActual;
+    protected int velocidadMaxima;
+
+    public Vehiculo(int velocidadActual, int velocidadMaxima){
+        this.velocidadActual = velocidadActual;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+    public abstract void acelerar(int velocidad);
+
+    public abstract void frenar(int velocidad);
+
+    public void imprimir(){
+        System.out.println("La velocidad actual es de: " + velocidadActual + "km/h");
+        System.out.println("La velocidad máxima es de: " + velocidadMaxima + "km/h");
+    }
+
 }
